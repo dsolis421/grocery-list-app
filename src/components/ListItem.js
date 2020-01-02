4b6ed5621fa7b4716e0006288719b069587d9b12
dsolis421 <danny@march76.com>
2020-01-01 19:58:56 -0600

    basic listing with components, no routing or backend

---
+++ src/components/ListItem.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ListItem extends Component {
+  render() {
+    return (
+      <div className="list-item">
+        <p>Here's something I need</p>
+      </div>
+    );
+  }
+}
+export default ListItem;
